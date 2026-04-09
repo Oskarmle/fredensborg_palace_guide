d0338f768f775dfc36028d36b565fb789de21ce8
OskarEriksen <omle.eriksen@gmail.com>
2026-04-09 12:07:41 +0200

    feat: reduced participants number

--- src/components/information/Important.tsx
+++ src/components/information/Important.tsx
@@ -6,7 +6,7 @@ export default function Important() {
       <h2 className={styles.important_headline}>Important!</h2>
       <p className={styles.important_text}>
         Please send a text to confirm you participate, as there are limited
-        spaces available (max. 18 participants).
+        spaces available (max. 10 participants).
       </p>
       <p className={styles.important_text}>
         Provisions are made for illness and other unforeseen situations.
