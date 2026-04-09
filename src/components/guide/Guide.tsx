4408157ebdce27d9328ff4f8d06f8e3df8b3f237
OskarEriksen <omle.eriksen@gmail.com>
2026-04-09 12:07:51 +0200

    feat: new image of guide

--- src/components/guide/Guide.tsx
+++ src/components/guide/Guide.tsx
@@ -1,12 +1,12 @@
 import styles from "./guide.module.css";
-import guideImage from "../../assets/guide.webp";
+import guideImage from "../../assets/guide_image.jpg";
 
 export default function Guide() {
   return (
     <aside className={styles.guide_container}>
       <img
         src={guideImage}
-        alt="Local guide at Fredensborg Palace"
+        alt="Local guide at Fredensborg Palace Garden"
         className={styles.guide_img}
       />
       <p className={styles.guide_name}>Guide: Henrik Riise</p>
